21f86b7430a4aa2dcbb92159f3d5e948cd6a4b2b
stringie <anthony.kaloferov@gmail.com>
2018-06-18 15:33:47 +0300

    Accidentally pushed topic 6 to topic5 branch

--- topic6/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Ownable = artifacts.require('../contracts/Ownable.sol');
-var PeopleBalances = artifacts.require('../contracts/PeopleBalances.sol')
-
-module.exports = function(deployer) {
-    deployer.deploy(Ownable);
-    deployer.deploy(PeopleBalances);
-}
